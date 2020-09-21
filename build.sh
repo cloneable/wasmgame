082f43e1dfa4cd738d81ec1ddfe0a9664402eae4
Folke Behrens <folke@gmail.com>
2020-09-21 15:45:37 +0200

    Make build script a bit smarter. Switch to 'debug' as default.

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+set -e
+
+BUILD_TYPE=debug
+BUILD_FLAGS=
+BUILD_OPT=false
+if [ "$1" == "--release" ]; then
+  BUILD_TYPE=release
+  BUILD_FLAGS=--release
+  BUILD_OPT=true
+fi
+
+cargo build $BUILD_FLAGS --target=wasm32-unknown-unknown
 rm -rf wbg
 mkdir wbg
-
-cargo build --release --target=wasm32-unknown-unknown
-wasm-bindgen --target web --out-dir wbg --no-typescript ./target/wasm32-unknown-unknown/release/wasmgame.wasm
-wasm-opt -O4 -o wbg/wasmgame_bg_opt.wasm wbg/wasmgame_bg.wasm
-wasm-strip wbg/wasmgame_bg_opt.wasm
-mv wbg/wasmgame_bg_opt.wasm wbg/wasmgame_bg.wasm
+wasm-bindgen --target web --out-dir wbg --no-typescript ./target/wasm32-unknown-unknown/"$BUILD_TYPE"/wasmgame.wasm
+if [ "$BUILD_OPT" == true ]; then
+  wasm-opt -O4 -o wbg/wasmgame_bg_opt.wasm wbg/wasmgame_bg.wasm
+  wasm-strip wbg/wasmgame_bg_opt.wasm
+  mv wbg/wasmgame_bg_opt.wasm wbg/wasmgame_bg.wasm
+fi
 cp -f wbg/wasmgame_bg.wasm web/wasmgame_bg.wasm
 cp -f wbg/wasmgame.js web/wasmgame.js
