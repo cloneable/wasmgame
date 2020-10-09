ab01b7839f89d64688fbeb27a58e52fc8f8a32ab
Folke Behrens <folke@gmail.com>
2020-10-09 22:47:32 +0200

    Finish the loading part of the macro.

--- app/src/game/assets.rs
+++ app/src/game/assets.rs
@@ -1,4 +1,4 @@
 static WEIRD_CUBE_OBJ: &'static [u8] = include_bytes!("../../assets/cube.obj");
 static WEIRD_CUBE_MTL: &'static [u8] = include_bytes!("../../assets/cube.mtl");
 
-static X: &'static [f32] = ::wasmgame_macros::load_obj!("app/assets/cube.obj");
+static X: &'static str = ::wasmgame_macros::load_obj!("app/assets/cube.obj");
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,10 +1,7 @@
 #![no_implicit_prelude]
 
 use ::std::convert::From;
-use ::std::fs::File;
-use ::std::io::Read;
 use ::std::result::{Result::Err, Result::Ok};
-use ::std::string::String;
 
 use ::proc_macro::TokenStream;
 use ::syn::parse::{Parse, ParseStream};
@@ -27,22 +24,13 @@ impl Parse for Input {
     }
 }
 
-fn process_input(input: Input) -> Result<()> {
+fn process_input(input: Input) -> Result<::obj::Obj> {
     let path = input.obj_path.value();
-    let mut data = String::new();
-    {
-        let cwd = ::std::env::current_dir()
-            .map_err(|err| ::syn::Error::new(input.obj_path.span(), err))?;
-        let mut f = File::open(cwd.join(path))
-            .map_err(|err| ::syn::Error::new(input.obj_path.span(), err))?;
-        f.read_to_string(&mut data)
-            .map_err(|err| ::syn::Error::new(input.obj_path.span(), err))?;
-    }
-    let buf = ::std::io::BufReader::new(data)
-    let o: ::obj::Obj = ::obj::load_obj(buf)
+    let cwd = ::std::env::current_dir()
         .map_err(|err| ::syn::Error::new(input.obj_path.span(), err))?;
-
-    Ok(())
+    let o: ::obj::Obj = ::obj::Obj::load(cwd.join(path))
+        .map_err(|err| ::syn::Error::new(input.obj_path.span(), err))?;
+    Ok(o)
 }
 
 #[proc_macro]
