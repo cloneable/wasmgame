2fd2385082d9dcbbb9c11e363db2b3963d766ef0
Folke Behrens <folke@gmail.com>
2020-10-10 23:13:15 +0200

    Fresh build with embedded objects.

--- web/wasmgame.js
+++ web/wasmgame.js
@@ -725,7 +725,7 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper688 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper685 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_30);
         return addHeapObject(ret);
     };
