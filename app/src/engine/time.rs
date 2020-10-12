d2883f6022e28f94ecb54b16e2e7e230418a29aa
Folke Behrens <folke@gmail.com>
2020-10-12 15:06:45 +0200

    Make tiles spin up and down.

--- app/src/engine/time.rs
+++ app/src/engine/time.rs
@@ -7,10 +7,24 @@ impl Time {
     }
 }
 
-impl ::std::ops::Sub for Time {
+impl ::std::ops::Add<Duration> for Time {
+    type Output = Time;
+    fn add(self, d: Duration) -> Time {
+        Time(self.0 - d.0)
+    }
+}
+
+impl ::std::ops::Sub<Time> for Time {
     type Output = Duration;
     fn sub(self, other: Time) -> Duration {
         Duration(self.0 - other.0)
+    }
+}
+
+impl ::std::ops::Rem<Duration> for Time {
+    type Output = Duration;
+    fn rem(self, divisor: Duration) -> Duration {
+        Duration(self.0 % divisor.0)
     }
 }
 
@@ -20,6 +34,31 @@ pub struct Duration(f64);
 impl Duration {
     pub fn from_millis(millis: f64) -> Self {
         Duration(millis)
+    }
+
+    pub fn as_pi(&self, period: Duration) -> f64 {
+        self.0 / period.0 * ::std::f64::consts::PI
+    }
+}
+
+impl ::std::ops::Add<Duration> for Duration {
+    type Output = Duration;
+    fn add(self, other: Duration) -> Duration {
+        Duration(self.0 + other.0)
+    }
+}
+
+impl ::std::ops::Mul<Duration> for f64 {
+    type Output = Duration;
+    fn mul(self, d: Duration) -> Duration {
+        Duration(self * d.0)
+    }
+}
+
+impl ::std::ops::Mul<f64> for Duration {
+    type Output = Duration;
+    fn mul(self, d: f64) -> Duration {
+        Duration(self.0 * d)
     }
 }
 
--- app/src/game/models.rs
+++ app/src/game/models.rs
@@ -4,7 +4,7 @@ use ::std::result::{Result, Result::Ok};
 
 use super::assets;
 use crate::engine;
-use crate::engine::time::Time;
+use crate::engine::time::{Duration, Time};
 use crate::util::math::Vec4;
 use crate::util::opengl::Context;
 use engine::scene::Model;
@@ -27,10 +27,8 @@ impl engine::scene::Drawable for Hexatile {
         self.model[0].object.translate([0.0, 0.55, 0.0].into());
         self.model[0].color(Vec4::with_rgb(0x19, 0x19, 0x70)); // midnightblue
 
-        self.model[1].object.rotate([0.0, 0.0, 0.0].into());
         self.model[1].color(Vec4::with_rgb(0x87, 0xce, 0xfa)); // lightskyblue
 
-        self.model[2].object.rotate([0.0, 30.0, 0.0].into());
         self.model[2].object.translate([0.0, -0.55, 0.0].into());
         self.model[2].color(Vec4::with_rgb(0xff, 0xb6, 0xc1)); // lightpink
 
@@ -38,9 +36,19 @@ impl engine::scene::Drawable for Hexatile {
     }
 
     fn update(&mut self, t: Time) {
-        self.model[0].object.rotate([0.0, 1.0, 0.0].into());
-        self.model[1].object.rotate([0.0, -1.0, 0.0].into());
-        self.model[2].object.rotate([0.0, 1.0, 0.0].into());
+        let period = Duration::from_millis(3000.0);
+        let offset = Duration::from_millis(500.0);
+
+        let rad = ((t + 0.0 * offset) % period).as_pi(period);
+        let deg1 = 3.0 * (rad as f32).sin();
+        let rad = ((t + 0.5 * offset) % period).as_pi(period);
+        let deg2 = 3.0 * (rad as f32).sin();
+        let rad = ((t + 1.0 * offset) % period).as_pi(period);
+        let deg3 = 3.0 * (rad as f32).sin();
+
+        self.model[0].object.rotate([0.0, deg1, 0.0].into());
+        self.model[1].object.rotate([0.0, deg2, 0.0].into());
+        self.model[2].object.rotate([0.0, deg3, 0.0].into());
         self.model.update(t);
     }
 
